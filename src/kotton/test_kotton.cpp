2968883929b8453c7544eab6ac53ed4609ac8c86
Kaiwalya Kher <knkher@gmail.com>
2014-03-12 04:44:44 -0700

    Refactoring things to be under the same Xcode project.

--- src/kotton/test_kotton.cpp
+++ src/kotton/test_kotton.cpp
@@ -1,18 +1,10 @@
-//
-//  main.cpp
-//  kluster-cli
-//
-//  Created by Kaiwalya Kher on 3/9/14.
-//  Copyright (c) 2014 Kaiwalya Kher. All rights reserved.
-//
 
 #include <iostream>
-#include <thread>
-#include <assert.h>
 #include "kotton/kotton.hpp"
 
 int main(int argc, const char * argv[])
 {
+
 	using namespace kotton;
 	create()->delegate("main", [](){
 		auto subscribe = self()->delegate("main", [](){
@@ -28,7 +20,5 @@ int main(int argc, const char * argv[])
 		
 		self()->unbecome();
 	});
-	
-	
 }
 
